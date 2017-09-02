4f81af8382285ad5f576b9173db22bd8e839a605
Amit Kulkarni <1408266+amit-kulkarni@users.noreply.github.com>
2017-09-02 15:42:20 +0530

    Cleaned up footer CSS  and alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      <span> Copyright &copy; None </span>
-      <a href="#" >Terms and Conditions</a>
+    <footer className="container">
+      <div className="row justify-content-center py-2" style={{textAlign: "center"}}>
+        <div className="col-12 col-md-3 my-1"> Copyright &copy; None </div>
+        <div className="col-12 col-md-3 my-1"><a href="#">Terms and Conditions</a></div>
+        <div className="col-12 col-md-3 my-1">Twitter</div>
+        <div className="col-12 col-md-3 my-1">Facebook</div>
+      </div>
     </footer>
   )
 }
